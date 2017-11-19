0299a947ac5c27305a372f6edd74f964bc83ece6
Thomas Delaet <thomas@delaet.org>
2017-11-19 16:29:56 +0100

    update

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-var GCM = require("./node-gcm-ccs.js");
-var uuid = require("node-uuid");
+var GCM = require('node-gcm-ccs')
+var uuid = require('node-uuid')
 
-var projectId = process.env.PROJECT_ID; 
-var apiKey = process.env.API_KEY;
+var projectId = process.env.PROJECT_ID
+var apiKey = process.env.API_KEY
 
-var gcm = new GCM(projectId, apiKey);
+var gcm = new GCM(projectId, apiKey)
 
-var queue = {};
+var queue = {}
 
-gcm.on('message', function(messageId, from, category, data) {
-    console.log(data);
-    if(data.type && data.type === "MESSAGE") {
-        if(data.destination && data.data) {
-            data.source = from;
-            send(data.destination, uuid.v4(), data, false);
-        } else {
-            console.log("incorrect parameters");
-        };
+gcm.on('message', function (messageId, from, category, data) {
+  console.log(data)
+  if (data.type && data.type === 'MESSAGE') {
+    if (data.destination && data.data) {
+      data.source = from
+      send(data.destination, uuid.v4(), data, false)
     } else {
-        console.log("unrecognized data type");
-        console.log(data);
-    };
-});
+      console.log('incorrect parameters')
+    }
+  } else {
+    console.log('unrecognized data type')
+    console.log(data)
+  }
+})
 
-var send = function(destination, id, data, receipt) {
-    var options = {};
-    options.messageId = id;
-    options.time_to_live = 0;
-    options.delay_while_idle = false;
-    options.delivery_receipt_requested = receipt;
-    console.log("sending message");
-    console.log(data);
-    gcm.send(destination, data, options);
-};
+var send = function (destination, id, data, receipt) {
+  var options = {}
+  options.messageId = id
+  options.time_to_live = 0
+  options.delay_while_idle = false
+  options.delivery_receipt_requested = receipt
+  console.log('sending message')
+  console.log(data)
+  gcm.send(destination, data, options)
+}
 
-gcm.on('connected', function() {
-    console.log("received connected event");
-});
+gcm.on('connected', function () {
+  console.log('received connected event')
+})
 
-gcm.on("disconnected", function() {
-    console.log("received disconnected event");
-    gcm.connect();
-});
+gcm.on('disconnected', function () {
+  console.log('received disconnected event')
+  gcm.connect()
+})
 
-gcm.on("error", function(error) {
-    console.log("received error event");
-    console.log(error);
-});
+gcm.on('error', function (error) {
+  console.log('received error event')
+  console.log(error)
+})
 
-gcm.on('message-error', function(message) {
-    console.log("received message error event");
-    console.log(message);
-});
+gcm.on('message-error', function (message) {
+  console.log('received message error event')
+  console.log(message)
+})
